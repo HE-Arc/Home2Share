a0f91c0308983a33a7213abf35d8dcf5e09d4703
Fleury Anthony <anth.fleury@gmail.com>
2018-02-15 18:01:04 +0100

    merge

--- Home2ShareProject/main/models.py
+++ Home2ShareProject/main/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class House(models.Model):
@@ -30,3 +34,14 @@ class House(models.Model):
 
     def __str__(self):
         return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_confirmed = models.BooleanField(default=False)
+    # other fields...
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
