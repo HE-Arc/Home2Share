105b2409031629acd286439d7bd764ef733a2129
Fleury Anthony <anth.fleury@gmail.com>
2018-03-03 13:40:04 +0100

    Basic comment system for houses
    Only new comment, no edition or deletion

--- Home2ShareProject/main/admin.py
+++ Home2ShareProject/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import House, Profile
+from .models import House, Profile, Comment
 
 # Register your models here.
 admin.site.register(House)
 admin.site.register(Profile)
+admin.site.register(Comment)
--- Home2ShareProject/main/urls.py
+++ Home2ShareProject/main/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('user/<slug>/houses', views.UserHouseListView.as_view(), name='user-house-list'),
     path('user/<slug>/update', views.UpdateUserView.as_view(), name='user-update'),
     path('user/<slug>/house/new', views.HouseCreateView.as_view(), name='house-new'),
-    
+    path('house/<slug>/comment/', views.HouseCommentCreateView.as_view(), name='house-comment-create'),
     path('house/<slug>/update', views.HouseUpdateView.as_view(), name='house-update'),
     path('house/<slug>/delete', views.HouseDeleteView.as_view(), name='house-delete'),
     path('register/',views.UserCreateView.as_view(),name='register'),
