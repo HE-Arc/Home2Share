896dba1f1950611912472e1f08c280a67cf97f4d
Cédric Pahud <cedric.pahud@he-arc.ch>
2018-03-28 15:36:07 +0200

    repared the requirements and changed the settings

--- Home2ShareProject/Home2ShareProject/settings.py
+++ Home2ShareProject/Home2ShareProject/settings.py
@@ -79,13 +79,24 @@ WSGI_APPLICATION = 'Home2ShareProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG :
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ.get('GROUPNAME'),
+            'USER': os.environ.get('GROUPNAME', 'postgres'),
+            'PASSWORD': os.environ.get('PASSWORD', ''),
+            'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+            'PORT': os.environ.get('POSTGRES_PORT', '5432')
+        }
+    }
 
 
 # Password validation
