a6c24c18e564e8c961ff233aa8700b3a0dc295d4
Fleury Anthony <anth.fleury@gmail.com>
2018-04-06 20:59:30 +0200

    Added Fixtures, Ordered comments, and rounded avg

--- Home2ShareProject/Home2ShareProject/settings.py
+++ Home2ShareProject/Home2ShareProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(ep+2e@(y494t*m^bb@q#22_(%h#r=i@9c41$lq7sg0w=3g6*^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['home2share.srvz-webapp.he-arc.ch', 'localhost']
 
